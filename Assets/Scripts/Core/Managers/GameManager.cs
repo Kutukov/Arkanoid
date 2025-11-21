1698e766fb39283093f1d07571fa639faa1a45d6
Kutukov <aleksa462808@mail.ru>
2025-11-21 23:45:43 +0700

    fixGameManager

--- Assets/Scripts/Core/Managers/GameManager.cs
+++ Assets/Scripts/Core/Managers/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject ballPrefab;
     [SerializeField] private Transform ballParent;
     [SerializeField] private int lives = 3;
-    [SerializeField] private Vector2 SpawnBallLocation = new Vector2(0, 0f);
+    [SerializeField] private Vector2 spawnBallLocation = new Vector2(0, 0f);
     [SerializeField] private TextMeshProUGUI livesText;
     [SerializeField] private TextMeshProUGUI scoreText;
 
@@ -20,101 +20,109 @@ public class GameManager : MonoBehaviour
     private Ball currentBall;
     private ObjectPool<Ball> ballPool;
 
-    private void Start()
-    {
-        StartGame();
-    }
     private void Awake()
     {
         if (Instance != null && Instance != this) Destroy(gameObject);
         Instance = this;
-        ballPool = new ObjectPool<Ball>(ballPrefab.GetComponent<Ball>(), ballParent, 1);
 
+        ballPool = new ObjectPool<Ball>(ballPrefab.GetComponent<Ball>(), ballParent, 1);
     }
 
     private void OnEnable()
     {
         GameEvents.OnScoreChanged += OnScoreChanged;
         GameEvents.OnBallLost += OnBallLost;
-        GameEvents.OnPauseGame += PauseGame;
-        GameEvents.OnLevelCleared += WinGame;
+        GameEvents.OnPauseGame += TogglePause;
+        GameEvents.OnLevelCleared += GameWin;
     }
 
     private void OnDisable()
     {
         GameEvents.OnScoreChanged -= OnScoreChanged;
         GameEvents.OnBallLost -= OnBallLost;
-        GameEvents.OnPauseGame -= PauseGame;
-        GameEvents.OnLevelCleared -= WinGame;
+        GameEvents.OnPauseGame -= TogglePause;
+        GameEvents.OnLevelCleared -= GameWin;
+    }
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void SetState(GameState newState)
+    {
+        if (State == newState) return;
+
+        State = newState;
+
+        switch (State)
+        {
+            case GameState.Playing:
+                Time.timeScale = 1f;
+                break;
+            case GameState.Paused:
+            case GameState.GameOver:
+            case GameState.Menu:
+                Time.timeScale = 0f;
+                break;
+        }
+
+    }
+
+    public void TogglePause()
+    {
+        if (State == GameState.Playing)
+            SetState(GameState.Paused);
+        else if (State == GameState.Paused)
+            SetState(GameState.Playing);
     }
 
     public void StartGame()
     {
         score = 0;
         lives = 3;
-        State = GameState.Playing;
-        SpawnBall(SpawnBallLocation);
+
+        SetState(GameState.Playing);
+        SpawnBall(spawnBallLocation);
         GameEvents.RaiseGameStarted();
         UpdateUI();
-    }
-
-    public void PauseGame()
-    {
-        Debug.Log("Pause");
-        State = GameState.Paused;
-        Time.timeScale = 0f;
-        // broadcast to pausable systems
-    }
-
-    public void ResumeGame()
-    {
-        State = GameState.Playing;
-        Time.timeScale = 1f;
-    }
-
-    private void OnScoreChanged(int delta)
-    {
-        score += delta;
-        UpdateUI();
-
     }
 
     private void OnBallLost()
     {
+        if (State != GameState.Playing) return;
+
         lives--;
         UpdateUI();
+
         if (lives <= 0) EndGame();
-        else SpawnBall(SpawnBallLocation);
+        else SpawnBall(spawnBallLocation);
     }
 
     private void EndGame()
     {
-        State = GameState.GameOver;
+        SetState(GameState.GameOver);
         GameEvents.RaiseGameOver();
-        GameOver();
-
-    }
-    private void GameOver()
-    {
-
-        StartGame();
     }
 
-
-    private void SpawnBall(Vector2 pos)
-    {
-        currentBall = ballPool.Get();
-        currentBall.transform.position = pos;
-        currentBall.gameObject.SetActive(true);
-
-    }
-    private void WinGame()
+    private void GameWin()
     {
         lives = 3;
-        State = GameState.Playing;
-        currentBall.gameObject.SetActive(false);
-        SpawnBall(SpawnBallLocation);
+        SetState(GameState.Playing);
+
+        if (currentBall)
+            currentBall.gameObject.SetActive(false);
+
+        SpawnBall(spawnBallLocation);
         GameEvents.RaiseGameStarted();
+        UpdateUI();
+    }
+
+    private void OnScoreChanged(int delta)
+    {
+        if (State != GameState.Playing) return;
+
+        score += delta;
         UpdateUI();
     }
 
@@ -122,5 +130,12 @@ public class GameManager : MonoBehaviour
     {
         if (livesText) livesText.text = $"Жизни: {lives}";
         if (scoreText) scoreText.text = $"Очки: {score}";
+    }
+
+    private void SpawnBall(Vector2 pos)
+    {
+        currentBall = ballPool.Get();
+        currentBall.transform.position = pos;
+        currentBall.gameObject.SetActive(true);
     }
 }
